fc5ccb3a7bc9121be8209a76a41eb513492d2885
Lachlan <lachyhughes@bigpond.com>
2024-09-12 11:08:50 +0930

    it should work

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: process.env.PORT || 3000,
     open: true,
     proxy: {
       '/assets': {
-        target: 'http://localhost:3001',
+        target: process.env.BACKEND_URL || 'http://localhost:3001',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/assets/, '/assets'),
       },
